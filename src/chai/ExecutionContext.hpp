878c210064442e19d4fca2403a1561ae0a9ce7c2
Alan Dayton <dayton8@llnl.gov>
2026-09-15 16:03:51 -0700

    Fix potential shared library issues

--- src/chai/ExecutionContext.hpp
+++ src/chai/ExecutionContext.hpp
@@ -7,84 +7,29 @@
 #ifndef CHAI_ExecutionContext_HPP
 #define CHAI_ExecutionContext_HPP
 
-#include "chai/DeviceHelpers.hpp"
-#include "chai/ExecutionSpaces.hpp"
+#include "chai/Types.hpp"
 
 namespace chai
 {
 namespace detail
 {
 
-inline thread_local ExecutionSpace execution_space = NONE;
-inline thread_local bool device_synchronized = true;
+CHAISHAREDDLL_API bool syncIfNeeded();
+
+CHAISHAREDDLL_API void setExecutionSpace(ExecutionSpace space);
+
+CHAISHAREDDLL_API ExecutionSpace getExecutionSpace();
+
+CHAISHAREDDLL_API bool isDeviceSynchronized();
+
+CHAISHAREDDLL_API void setDeviceSynchronized(bool synchronized);
+
+CHAISHAREDDLL_API void resetExecutionContext();
 
 #if defined(CHAI_ENABLE_GPU_SIMULATION_MODE)
-inline bool gpu_sim_mode = false;
-#endif
+CHAISHAREDDLL_API void setGPUSimMode(bool enabled);
 
-inline bool syncIfNeeded()
-{
-  if (!device_synchronized) {
-    synchronize();
-    device_synchronized = true;
-    return true;
-  }
-
-  return false;
-}
-
-inline void setExecutionSpace(ExecutionSpace space)
-{
-#if defined(CHAI_ENABLE_GPU_SIMULATION_MODE)
-  if (gpu_sim_mode && space != NONE) {
-    space = GPU;
-  }
-#endif
-
-  if (space == GPU) {
-    device_synchronized = false;
-  }
-
-#if defined(CHAI_THIN_GPU_ALLOCATE)
-  if (space == CPU) {
-    syncIfNeeded();
-  }
-#endif
-
-  execution_space = space;
-}
-
-inline ExecutionSpace getExecutionSpace()
-{
-  return execution_space;
-}
-
-inline bool isDeviceSynchronized()
-{
-  return device_synchronized;
-}
-
-inline void setDeviceSynchronized(bool synchronized)
-{
-  device_synchronized = synchronized;
-}
-
-inline void resetExecutionContext()
-{
-  execution_space = NONE;
-  device_synchronized = true;
-}
-
-#if defined(CHAI_ENABLE_GPU_SIMULATION_MODE)
-inline void setGPUSimMode(bool enabled)
-{
-  gpu_sim_mode = enabled;
-}
-
-inline bool isGPUSimMode()
-{
-  return gpu_sim_mode;
-}
+CHAISHAREDDLL_API bool isGPUSimMode();
 #endif
 
 }  // namespace detail
